f348b28dafe42f59b7c899d062061eb3619daa2f
Sidney Brooks <91339089+articsid@users.noreply.github.com>
2026-05-06 12:05:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         UI ui = new UI();
+        //Benchmark.run(ui.getManager());
         ui.start();
     }
 }
